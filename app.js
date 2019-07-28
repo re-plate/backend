9961f2706a291734b8718e3aa6870fd87b0ca0f1
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-28 18:44:20 +0100

    install express to setup server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log(`app listening on port 3000`))
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
