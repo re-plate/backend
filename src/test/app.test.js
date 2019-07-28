2f5e1ce7a39fad3210b617d6456a937edf636191
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-28 20:58:02 +0100

    install mocha and chai dependencies

---
+++ src/test/app.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Sample Test', () => {
+  it('should return -1 when the value is not present', () => {
+    const me = 1;
+    expect(me).to.equal(1);
+  });
+});
