48b07915a3e4d40ffa16fcc2da0a48c2ed0f161e
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-29 20:58:10 +0100

    add requests migration

---
+++ src/data/migrations/20190729204428_requests.js
@@ -0,0 +1,19 @@
+exports.up = knex => knex.schema.createTable('requests', (tbl) => {
+  tbl.increments();
+  tbl.text('name', 128).notNullable();
+  tbl.text('food_type', 128).notNullable();
+  tbl.text('pickup_date', 128).notNullable();
+  tbl.text('pickup_time', 128).notNullable();
+  tbl.text('comment', 128);
+  tbl.text('instruction', 128);
+  tbl
+    .integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+});
+
+exports.down = knex => knex.schema.dropTableIfExists('requests');
