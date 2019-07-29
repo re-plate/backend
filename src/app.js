3a5ca59142cc06a6cd6a73dae85e316ce1d93a15
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-29 16:02:50 +0100

    add cors and morgan dependencies

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 import authRoutes from './routes/auth';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to Replate API 👋🏾' });
