8514616089f7bbfd0a8fb344ada039873837bc91
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-31 14:16:20 +0100

    integrate twillio to send sms to users

--- src/services/Sms.js
+++ src/services/Sms.js
@@ -7,16 +7,21 @@ const {
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
   TWILIO_SENDER_NUMBER,
+  NODE_ENV,
 } = process.env;
 
 const twilioClient = client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const sendMessage = (phone, message) => twilioClient.messages
-  .create({
-    body: message,
-    from: TWILIO_SENDER_NUMBER,
-    to: phone,
-  })
-  .then(res => res);
+const sendMessage = (phone, message) => {
+  if (NODE_ENV === 'production') {
+    twilioClient.messages
+      .create({
+        body: message,
+        from: TWILIO_SENDER_NUMBER,
+        to: phone,
+      })
+      .then(res => res);
+  }
+};
 
 export default sendMessage;
